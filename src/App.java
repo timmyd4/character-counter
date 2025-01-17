6f4defa9e64f86a269cd107dd539f7dbbd6f2212
Tim <timmywilliams4665@gmail.com>
2025-01-16 18:38:02 -0800

    Finished at home with the help of the video

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) {
+public class App
+{
+    public static void main(String[] args)
+    {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("\nEnter a string to analyze:");
--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -2,6 +2,27 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class CharacterFrequencyCounterTest {
-    
+public class CharacterFrequencyCounterTest
+{   
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld()
+    {
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("helloworld");
+        //Act 
+        int actualCount = counter.getFrequency('l');
+        //Assert
+        assertEquals(3, actualCount);
+    }   
+
+    @Test
+    public void testNonOccuringCharacterPercentageIsZero()
+    {   
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("hi there");
+        //Act
+        double actualPercent = counter.getRelativePercentage('z');
+        //Assert
+        assertEquals(0.0, actualPercent, 0);
+    }
 }
